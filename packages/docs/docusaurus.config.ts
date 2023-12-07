3d072f519dca4672b53439752737f1bdd8ab7821
livemehere <rhdxoals@gmail.com>
2023-12-08 04:34:26 +0900

    🚀 initial deploy done

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   url: "https://livemehere.github.io",
-  baseUrl: "/",
+  baseUrl: "/livemehere-dev-packs/",
   organizationName: "livemehere",
   projectName: "livemehere-dev-packs",
   trailingSlash: false,
