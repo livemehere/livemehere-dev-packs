ce7d6d2381e1f3fc09e3d77ca3cf8e016d176910
livemehere <rhdxoals@gmail.com>
2023-12-08 04:22:15 +0900

    🔧 fix config

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   url: "https://livemehere.github.io",
-  baseUrl: "/livemehere-dev-packs/",
+  baseUrl: "/",
   organizationName: "livemehere",
   projectName: "livemehere-dev-packs",
   trailingSlash: false,
